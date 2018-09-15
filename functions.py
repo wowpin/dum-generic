1975ace344e78affbe95968b8e37f387c4bef943
wowpin <30692035+wowpin@users.noreply.github.com>
2018-09-15 20:50:14 +0200

    Expanded the log() function
    Logging function now creates a log file in addition to logging to stdout.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 # Function for returning a first available key value for appending a new element to a dictionary
 def getFreeKey(itemsDict):
@@ -9,6 +10,14 @@ def getFreeKey(itemsDict):
     except Exception:
         pass
     return(x)
-	
+
+# Function used for loggin messages to stdout and a disk file
 def log(content, type):
-	print(str(time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + " [" + type + "] " + content))
+    logfile = 'dum.log'
+    print(str(time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + " [" + type + "] " + content))
+    if os.path.exists(logfile):
+        log = open(logfile, 'a')
+    else:
+        log = open(logfile, 'w')
+    log.write(str(time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + " [" + type + "] " + content) + '\n')
+    log.close()
